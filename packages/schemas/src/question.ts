d7badaf02e225702749c7e981660057da4899766
KtaK721 <138202999+KtaK721@users.noreply.github.com>
2024-07-17 23:12:28 +0900

    why does the error happen

--- packages/schemas/src/question.ts
+++ packages/schemas/src/question.ts
@@ -1,5 +1,4 @@
 import typia, { type tags } from "typia";
-import type { MaxLength, MinItems, MinLength } from "typia/lib/tags";
 
 // Example schema
 
@@ -12,8 +11,8 @@ export interface Board {
   readonly width: number & tags.Type<"uint32"> & tags.Maximum<256>;
   readonly height: number & tags.Type<"uint32"> & tags.Maximum<256>;
 
-  start: Array<string & tags.MinLength<1> & tags.MaxLength<256>> & MinItems<1> & tags.MaxItems<256>;
-  readonly goal: Array<string & tags.MinLength<1> & tags.MaxLength<256>> & MinItems<1> & tags.MaxItems<256>;
+  start: Array<string & tags.MinLength<1> & tags.MaxLength<256>> & tags.MinItems<1> & tags.MaxItems<256>;
+  readonly goal: Array<string & tags.MinLength<1> & tags.MaxLength<256>> & tags.MinItems<1> & tags.MaxItems<256>;
 }
 
 export interface General {
