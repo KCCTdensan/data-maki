1a56d756da80d0d053f2bc39334691c01a6a105b
Mido <mido@wycey.dev>
2024-07-17 22:21:41 +0900

    Revert "try to make question.ts ans answer.ts"

--- packages/schemas/src/question.ts
+++ packages/schemas/src/question.ts
@@ -1,29 +1,12 @@
 import typia, { type tags } from "typia";
-import type { MaxLength, MinItems, MinLength } from "typia/lib/tags";
 
 // Example schema
 
 export interface Question {
   board: Board;
-  general: General;
 }
 
 export interface Board {
-  readonly width: number & tags.Type<"uint32"> & tags.Maximum<256>;
-  readonly height: number & tags.Type<"uint32"> & tags.Maximum<256>;
-
-  start: Array<string & tags.MinLength<1> & tags.MaxLength<256>> & MinItems<1> & tags.MaxItems<256>;
-  readonly goal: Array<string & tags.MinLength<1> & tags.MaxLength<256>> & MinItems<1> & tags.MaxItems<256>;
-}
-
-export interface General {
-  readonly n: number & tags.Type<"uint32"> & tags.Maximum<256>;
-  readonly patterns: Array<Patterns> & tags.MaxItems<256>;
-}
-
-export interface Patterns {
-  readonly p: number & tags.Type<"uint32"> & tags.Maximum<281>;
-  readonly width: number & tags.Type<"uint32"> & tags.Maximum<256>;
-  readonly height: number & tags.Type<"uint32"> & tags.Maximum<256>;
-  readonly cells: Array<string & tags.MinLength<1> & tags.MaxLength<256>> & tags.MinItems<1> & tags.MaxItems<256>;
+  width: number & tags.Type<"uint32"> & tags.Minimum<32> & tags.Maximum<256>;
+  height: number & tags.Type<"uint32"> & tags.Minimum<32> & tags.Maximum<256>;
 }
