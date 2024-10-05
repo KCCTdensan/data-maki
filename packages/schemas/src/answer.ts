d815b84c805663722c22f583e886425f1f07e515
Mido <instructr13@gmail.com>
2024-10-05 19:10:49 +0900

    add answer schema
    Co-authored-by: KtaK721 <138202999+KtaK721@users.noreply.github.com>

--- packages/schemas/src/answer.ts
+++ packages/schemas/src/answer.ts
@@ -1,7 +1,13 @@
-import type { tags } from "typia";
-
-// Example schema
+import typia, { type tags } from "typia";
 
 export interface Answer {
-  n: number & tags.Type<"uint32">;
+  n: number & tags.Type<"uint32"> & tags.Maximum<320000>;
+  ops: Array<Ops> & tags.MaxItems<320000>;
+}
+
+export interface Ops {
+  p: number & tags.Type<"uint32"> & tags.Maximum<281>;
+  x: number & tags.Type<"int32"> & tags.Minimum<-255> & tags.Maximum<255>;
+  y: number & tags.Type<"int32"> & tags.Minimum<-255> & tags.Maximum<255>;
+  s: number & tags.Type<"uint32"> & tags.Maximum<3>;
 }
