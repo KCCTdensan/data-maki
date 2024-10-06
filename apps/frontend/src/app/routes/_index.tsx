e34b322319f1a5e8661de61e8d6e653f2f540998
Mido <instructr13@gmail.com>
2024-10-06 12:49:19 +0900

    remove SSR-only usecases

--- apps/frontend/src/app/routes/_index.tsx
+++ apps/frontend/src/app/routes/_index.tsx
@@ -1,9 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
 import { MainBoardView } from "../components/board/MainBoardView";
-
-export const meta: MetaFunction = () => {
-  return [{ title: "Data Maki UI" }];
-};
 
 export default function Index() {
   return (
