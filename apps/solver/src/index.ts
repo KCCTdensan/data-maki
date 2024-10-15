fc02b7443d2d339dd826e9969864e19b96a4dd61
GIGAHERTZ00 <gigahertz00@gmail.com>
2024-10-15 22:06:59 +0900

    ソルバーの並列化に関するあれこれ

--- apps/solver/src/index.ts
+++ apps/solver/src/index.ts
@@ -10,15 +10,33 @@ export type { SolverApp } from "./features/ui-comm";
 const main = async () => {
   const features = await bootstrap(_defaultLog);
 
-  const startPromise = Promise.all(
-    features.map((feature) =>
+  const combinations: [boolean, boolean, boolean][] = [
+    [false, false, false],
+    [false, false, true],
+    [false, true, false],
+    [false, true, true],
+    [true, false, false],
+    [true, false, true],
+    [true, true, false],
+    [true, true, true],
+  ];
+
+  const workerPromises = combinations.map(([rv_ul, rv_ud, rv_lr]) =>
+    solve_worker(rv_ul, rv_ud, rv_lr).catch((e) => {
+      throw new FeatureError(e, new Error(`solve_worker ${e} failed`)); // すまん、FeatureErrorの使い方分からんかったから第一引数を適当にeにした
+    }),
+  );
+
+  const startPromise = Promise.all([
+    ...features.map((feature) =>
       feature.start().catch((e) => {
         if (e instanceof Error) throw new FeatureError(feature, e);
 
         throw new FeatureError(feature, new Error(`Unknown error: ${e}`));
       }),
     ),
-  );
+    ...workerPromises,
+  ]);
 
   setTimeout(() => {
     StateManager.instance.setState(IdleState.instance);
